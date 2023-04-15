bd58959fb2e4ccf9db65dc95cbb74ff2830e1529
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2023-04-15 16:28:59 +0000

    Created Python test file to read csv files

---
+++ test.py
@@ -0,0 +1,5 @@
+import csv
+with open('GlobalTemperatures.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
