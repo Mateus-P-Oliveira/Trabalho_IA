cb8cbee6605228c2ed2fc3e55299a8fb0d739586
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2023-04-16 20:46:40 +0000

    Uso de Dicionario para a file

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 import csv
+
+#temperature = []
 with open('GlobalTemperatures.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
+    #data = csv.reader(csvfile, delimiter=',', quotechar='|')
+    data = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+    print("Date", ":", "Average Temperature")
+    for row in data:
+        #
+        #temperature.append(row['Temperature'])
+        #print("  |  ".join(row))
+        #print(row['dt'], ":", row['LandAverageTemperature'])
+        print(row) #Ver como converter para float
+
+#Fazer a media mensal 
+#Para trabalhar com meses ao inves de variação diaria 
