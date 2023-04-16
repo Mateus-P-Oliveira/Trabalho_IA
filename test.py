14f0f515164f188dc7b0972d675aece2be0e15db
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2023-04-16 20:58:25 +0000

    Atualizado Test.py

--- test.py
+++ test.py
@@ -1,16 +1,21 @@
 import csv
 
-#temperature = []
+temperature = []
 with open('GlobalTemperatures.csv', newline='') as csvfile:
     #data = csv.reader(csvfile, delimiter=',', quotechar='|')
     data = csv.DictReader(csvfile, delimiter=',', quotechar='|')
     print("Date", ":", "Average Temperature")
     for row in data:
-        #
+
+        temperature[row] = float(row['dt'])
         #temperature.append(row['Temperature'])
         #print("  |  ".join(row))
-        #print(row['dt'], ":", row['LandAverageTemperature'])
-        print(row) #Ver como converter para float
+        print(row['dt'], ":", row['LandAverageTemperature'])
+        #print(row) #Ver como converter para float
 
 #Fazer a media mensal 
 #Para trabalhar com meses ao inves de variação diaria 
+
+for sub in the_list:
+    for key in sub:
+        sub[key] = int(sub[key])
