77c1f83c77a81b25f585feb4347aa88a8e15cdaf
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2023-04-18 23:19:00 +0000

    Arrumei o X e o Y

--- test2.py
+++ test2.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+
 from sklearn.linear_model import LinearRegression
 df = pd.read_csv('GlobalTemperatures.csv')
 DateColumn = df['dt']
@@ -10,12 +12,21 @@ TemperatureColumn = df['LandAverageTemperature']
 
 tmp = DateColumn.str.split('-')
 DateColumn = (tmp.str[0]+tmp.str[1]+tmp.str[2]).astype(int)
-
+TemperatureColumn = TemperatureColumn.fillna(0)
 #print(DateColumn)
 #print(TemperatureColumn)
-reshape((-1, 1))
-model = LinearRegression()
-x= DateColumn
-model = LinearRegression().fit(DateColumn, TemperatureColumn)
+#reshape((-1, 1))
+#model = LinearRegression()
+x= DateColumn.to_numpy().reshape((-1,1))
+y= TemperatureColumn.to_numpy()
+
+#x.reshape((1,-1))
+print(x)
+print(y)
+
+model = LinearRegression().fit(x, y)
+#sns.histplot(model)
+r_sq = model.score(x, y)
+print(f"coefficient of determination: {r_sq}")
 #Fazer a media mensal  já tem
 #Para trabalhar com meses ao inves de variação diaria 
