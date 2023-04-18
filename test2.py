2ab4d97a4244f07fa27910510415ba42e2eb473e
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2023-04-18 19:06:08 +0000

    Feito a transformação em Int

--- test2.py
+++ test2.py
@@ -1,10 +1,21 @@
 import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
 df = pd.read_csv('GlobalTemperatures.csv')
 DateColumn = df['dt']
 TemperatureColumn = df['LandAverageTemperature']
 
-print(DateColumn)
-print(TemperatureColumn)
+#def to_integer(DateColumn):
+    #return 10000*DateColumn.year + 100*DateColumn.month + DateColumn.day
 
-#Fazer a media mensal 
+tmp = DateColumn.str.split('-')
+DateColumn = (tmp.str[0]+tmp.str[1]+tmp.str[2]).astype(int)
+
+#print(DateColumn)
+#print(TemperatureColumn)
+reshape((-1, 1))
+model = LinearRegression()
+x= DateColumn
+model = LinearRegression().fit(DateColumn, TemperatureColumn)
+#Fazer a media mensal  já tem
 #Para trabalhar com meses ao inves de variação diaria 
