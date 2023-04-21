e655ed566d3d8d9bb29035590eaa1859fc7027a7
Mateus Pinto de Oliveira <mateus.pinto@acad.pucrs.br>
2023-04-21 15:00:50 -0300

    Terminei a formatação da linha

--- test2.py
+++ test2.py
@@ -2,42 +2,31 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-
-
-
 from sklearn.linear_model import LinearRegression
-df = pd.read_csv('GlobalTemperatures.csv')
-DateColumn = df['dt']
+
+df = pd.read_csv('GlobalTemperatures.csv',parse_dates=['date'])
+DateColumn = df['date']
 TemperatureColumn = df['LandAverageTemperature']
 
-#def to_integer(DateColumn):
-    #return 10000*DateColumn.year + 100*DateColumn.month + DateColumn.day
 
-tmp = DateColumn.str.split('-')
-DateColumn = (tmp.str[2]+tmp.str[1]+tmp.str[0]).astype(int)
-TemperatureColumn = TemperatureColumn.fillna(0)
-#print(DateColumn)
-#print(TemperatureColumn)
-#reshape((-1, 1))
-#model = LinearRegression()
-x1 =DateColumn.to_numpy()
-y1= TemperatureColumn.to_numpy()
-#plt.scatter(x1, y1)
-#plt.show()
-x= DateColumn.to_numpy().reshape((-1,1))
-y= TemperatureColumn.to_numpy()
+  
+X =df.groupby(df.date.dt.year)['LandAverageTemperature'].mean()
+Y = X.tolist()
+X1 = X.index.tolist()
 
-#x.reshape((1,-1))
-print(x)
-print(y)
 
-model = LinearRegression().fit(x, y)
-#sns.histplot(model)
-r_sq = model.score(x, y)
-print(f"coefficient of determination: {r_sq}")
-plt.scatter(x, y)
+x = np.array(X1)
+y = np.array(Y)
+plt.scatter(x,y)
 plt.show()
+xreshape=x.reshape(-1,1)
+
+
+model = LinearRegression().fit(xreshape, y)
+
+r_sq = model.score(xreshape, y)
+print(f"coefficient of determination: {r_sq}")
+
 
 
 #Fazer a media mensal  já tem
